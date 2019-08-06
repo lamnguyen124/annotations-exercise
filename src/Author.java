f889229537325743eba5c33e8c73b26ca36e9905
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-08-06 14:59:27 -0500

    update

--- src/Author.java
+++ src/Author.java
@@ -5,32 +5,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Author extends Person {
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<String>();
     }
 
     /**
      * @deprecated Use addBook instead
      */
     @Deprecated
-    @SuppressWarnings("unchecked")
     public List<String> getBooks() {
         return books;
     }
 
-    @SuppressWarnings("unchecked")
     public List<String> publishedBooks() {
         return books;
     }
 
-    @SuppressWarnings("unchecked")
     public void addBook(String book) {
         books.add(book);
     }
 
+    @Override
     public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
